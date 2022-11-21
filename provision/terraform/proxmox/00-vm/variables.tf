59271e47082230d5b9713bdd55517f1384bb9c1a
vikaspogu <vikaspoguadf@gmail.com>
2022-11-21 09:59:56 -0600

    fix precommit lint

--- provision/terraform/proxmox/00-vm/variables.tf
+++ provision/terraform/proxmox/00-vm/variables.tf
@@ -1,15 +1,14 @@
 variable "pm_node" {
-  default = "proxmox-0"
-}
-
-variable "ssh_key_file" {
-  default = "~/.ssh/id_rsa.pub"
+  default     = "proxmox-0"
+  type        = string
+  description = "Proxmox node name"
 }
 
 variable "ssh_keys" {
-    type = map
-     default = {
-       pub  = "~/.ssh/id_rsa.pub"
-       priv = "~/.ssh/id_rsa"
-     }
+  type        = map(any)
+  description = "SSH key map public, private location"
+  default = {
+    pub  = "~/.ssh/id_rsa.pub"
+    priv = "~/.ssh/id_rsa"
+  }
 }
