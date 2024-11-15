3b945b245cc6c0e3b9146b85c74102211806fbfd
Vikas Pogu <vikaspoguadf@gmail.com>
2024-11-15 10:05:08 -0500

    (fix): backup config

--- kubernetes/apps/default/truenas-backup/app/truenas-backup.sh
+++ kubernetes/apps/default/truenas-backup/app/truenas-backup.sh
@@ -7,38 +7,16 @@ http_host=${S3_URL#*//}
 http_host=${http_host%:*}
 http_request_date=$(date -R)
 
-echo "Download TrueNAS pxm-odin config file ..."
-config_filename="pxm-odin-$(date "+%Y%m%d-%H%M%S").tar"
-http_filepath="truenas-backup/${config_filename}"
+echo "Download TrueNAS Main config file ..."
+config_filename="$(date "+%Y%m%d-%H%M%S").tar"
+http_filepath="truenas-main/${config_filename}"
 http_signature=$(
     printf "PUT\n\napplication/tar\n%s\n/%s" "${http_request_date}" "${http_filepath}" \
         | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary \
         | base64
 )
 curl -k -X 'POST' \
-  "https://${TRUENAS_PXM_ODIN_URL}/api/v2.0/config/save" \
-  -H 'accept: */*' \
-  -H 'Content-Type: application/json' --user "${TRUENAS_USER}:${TRUENAS_PASS}" --output "/tmp/${config_filename}"
-
-echo "Upload backup to s3 bucket ..."
-curl -fsSL \
-    -X PUT -T "/tmp/${config_filename}" \
-    -H "Host: ${http_host}" \
-    -H "Date: ${http_request_date}" \
-    -H "Content-Type: application/tar" \
-    -H "Authorization: AWS ${AWS_ACCESS_KEY_ID}:${http_signature}" \
-    "${S3_URL}/${http_filepath}"
-
-echo "Download TrueNAS proxmox config file ..."
-config_filename="proxmox-$(date "+%Y%m%d-%H%M%S").tar"
-http_filepath="truenas-backup/${config_filename}"
-http_signature=$(
-    printf "PUT\n\napplication/tar\n%s\n/%s" "${http_request_date}" "${http_filepath}" \
-        | openssl sha1 -hmac "${AWS_SECRET_ACCESS_KEY}" -binary \
-        | base64
-)
-curl -k -X 'POST' \
-  "https://${TRUENAS_PROXMOX_URL}/api/v2.0/config/save" \
+  "https://${TRUENAS_MAIN_URL}/api/v2.0/config/save" \
   -H 'accept: */*' \
   -H 'Content-Type: application/json' --user "${TRUENAS_USER}:${TRUENAS_PASS}" --output "/tmp/${config_filename}"
 
